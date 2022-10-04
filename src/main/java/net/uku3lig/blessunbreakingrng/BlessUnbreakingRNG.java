fddfc680050d858b1e5cd217197d3edd8e9b4d69
uku <uku3lig@gmail.com>
2022-10-04 14:45:40 +0200

    initial commit

---
+++ src/main/java/net/uku3lig/blessunbreakingrng/BlessUnbreakingRNG.java
@@ -0,0 +1,17 @@
+package net.uku3lig.blessunbreakingrng;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlessUnbreakingRNG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
